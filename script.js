63c48cf3931df23b1b927457965f17e8e0ef1345
Azamat Gaparov <79432713+agaparov-relay@users.noreply.github.com>
2022-03-25 21:16:47 -0400

    Latest changes

--- script.js
+++ script.js
@@ -6,11 +6,51 @@ import { cocktails } from './data.js';
 
 const main = document.querySelector('.main-content');
 const search = document.querySelector('#search');
+const searchBtn = document.querySelector('#search-btn');
 
 let cocktailsData = cocktails;
 
 function renderData(cocktails) {
-  console.log('a', cocktails);
+  main.innerHTML = '';
+  for (let item of cocktails) {
+    createCocktails(item);
+  }
 }
 
 renderData(cocktailsData);
+
+function createCocktails(item) {
+  const el = `
+    <div class="cocktail-item">
+      <div class="img-wrapper">
+        <img
+          src="${item.strDrinkThumb}"
+          alt=""
+        />
+        <p class="category-type">${item.strCategory}</p>
+      </div>
+      <p class="cocktail-name">
+        ${item.strDrink}
+        <span class="alcohol-type">${item.strAlcoholic}</span>
+      </p>
+    </div>
+  `;
+
+  main.innerHTML += el;
+}
+
+searchBtn.addEventListener('click', function () {
+  const name = search.value;
+  const filteredData = cocktailsData.filter((item) => {
+    return item.strDrink.toLowerCase().includes(name);
+  });
+  renderData(filteredData);
+});
+
+// search.addEventListener('input', function (event) {
+//   const name = event.target.value;
+//   const filteredData = cocktailsData.filter((item) => {
+//     return item.strDrink.toLowerCase().includes(name);
+//   });
+//   renderData(filteredData);
+// });
